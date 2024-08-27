c846d2dca25cb3acae45869ba23d9e06910aebe1
gun <rkdrjs71@naver.com>
2024-08-27 15:08:37 +0900

    init : 초기 커밋

---
+++ src/main/java/site/gun/emotion_calendar/EmotionCalendarApplication.java
@@ -0,0 +1,13 @@
+package site.gun.emotion_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/gun/emotion_calendar/EmotionCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package site.gun.emotion_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
